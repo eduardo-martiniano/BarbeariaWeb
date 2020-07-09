c478e0e9637245aa732f184ab605fde31ceac2a1
Eduardo Martiniano <eeduardo.martiniano@gmail.com>
2020-07-09 18:11:04 -0300

    Listando servicos agendados

--- BarbeariaWeb/Controllers/AgendaController.cs
+++ BarbeariaWeb/Controllers/AgendaController.cs
@@ -2,15 +2,31 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BarbeariaWeb.Database;
+using BarbeariaWeb.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BarbeariaWeb.Controllers
 {
     public class AgendaController : Controller
     {
+        private DatabaseContext _db;
+
+        public AgendaController(DatabaseContext db)
+        {
+            _db = db;
+        }
         public IActionResult Index()
         {
+            ViewBag.agendas = _db.Agendas.ToList();
             return View();
         }
+        [HttpGet]
+        public IActionResult NovaAgenda()
+        {
+            return View();
+
+        }
+
     }
 }
