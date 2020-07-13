83238409828aa63d39b85d685734dc4c817a7764
Eduardo Martiniano <eeduardo.martiniano@gmail.com>
2020-07-13 10:51:04 -0300

    Adcionando campo nome no agendamento

--- BarbeariaWeb/Models/Agenda.cs
+++ BarbeariaWeb/Models/Agenda.cs
@@ -8,6 +8,7 @@ namespace BarbeariaWeb.Models
     public class Agenda
     {
         public int Id { get; set; }
+        public string NomeCliente { get; set; }
         public string Servico { get; set; }
         public double Valor { get; set; }
         public string DataHora { get; set; }
